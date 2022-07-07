97abc1975c23e02590ca27b807b51ecbcc4d7cdf
Jonathansteven21 <jonathansteven.gc@gmail.com>
2022-07-07 18:11:58 -0500

    Contract test response has been verified

---
+++ test/schema/ListPublicEvents.schema.js
@@ -0,0 +1,10 @@
+const listPublicEventsSchema = {
+  number: 1,
+  bool: false,
+  arrays: [0, 1, 2],
+  strings: 'Example_String',
+  objects: {
+    Type: 'Object_Example'
+  }
+};
+exports.listPublicEventsSchema = listPublicEventsSchema;
